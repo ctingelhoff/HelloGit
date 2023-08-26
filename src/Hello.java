c9f9f71d3223b08bb9412e0d29ad806a7c891d6e
cting <cting@Caden_Laptop>
2023-08-26 17:13:53 -0500

    added my major to the output

--- src/Hello.java
+++ src/Hello.java
@@ -12,5 +12,6 @@ public class Hello {
 	
 	public static void main(String args[]) {
 		System.out.println("My name is Caden Tingelhoff");
+		System.out.println("My major is Computer Science");
 	}
 }
