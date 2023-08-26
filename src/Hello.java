7b60c944dc4710cee1cbb7550833ce3915429835
cting <cting@Caden_Laptop>
2023-08-26 17:05:49 -0500

    Initial Commit

---
+++ src/Hello.java
@@ -0,0 +1,16 @@
+/**
+ * Author: Caden Tingelhoff
+ * Date: 08/26/2023
+ * 
+ * A program that prints my name
+ * 
+ * @author ctingelhoff
+ * 
+ */
+
+public class Hello {
+	
+	public static void main(String args[]) {
+		System.out.println("My name is Caden Tingelhoff");
+	}
+}
